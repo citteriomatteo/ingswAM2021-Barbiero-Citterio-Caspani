8ffebcbb5db2c3cd881e55540ebd656ed899bfde
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2021-04-06 10:32:14 +0200

    Adder interface definition

--- src/main/java/it/polimi/ingsw/model/match/player/Adder.java
+++ src/main/java/it/polimi/ingsw/model/match/player/Adder.java
@@ -1,4 +1,16 @@
 package it.polimi.ingsw.model.match.player;
 
+import it.polimi.ingsw.model.essentials.PhysicalResource;
+
+/*
+    Interface used for limiting access to player methods
+    It contains methods for adding resources or faith points
+*/
 public interface Adder {
+
+    //This method increments player's faithPoints
+    boolean addFaithPoints(int quantity);
+
+    //This method adds resource to the strong box
+    boolean addToStrongBox(PhysicalResource resource);
 }
