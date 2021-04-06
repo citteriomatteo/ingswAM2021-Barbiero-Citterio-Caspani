e25d02ee2b8ee6a07d7b1e2a094cb6848225489b
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2021-04-06 17:46:29 +0200

    DevelopmentCard implementation

--- src/main/java/it/polimi/ingsw/model/essentials/DevelopmentCard.java
+++ src/main/java/it/polimi/ingsw/model/essentials/DevelopmentCard.java
@@ -1,5 +1,45 @@
 package it.polimi.ingsw.model.essentials;
 
-public class DevelopmentCard {
+import it.polimi.ingsw.model.match.player.Verificator;
 
+import java.util.List;
+
+public class DevelopmentCard {
+    private final List<PhysicalResource> price;
+    private final CardType type;
+    private final int winPoints;
+    private final Production production;
+
+    //Simple constructor
+    public DevelopmentCard(List<PhysicalResource> price, CardType type, int winPoints, Production production) {
+        this.price = price;
+        this.type = type;
+        this.winPoints = winPoints;
+        this.production = production;
+    }
+
+    public List<PhysicalResource> getPrice() {
+        return price;
+    }
+
+    public CardType getType() {
+        return type;
+    }
+
+    public int getWinPoints() {
+        return winPoints;
+    }
+
+    public Production getProduction() {
+        return production;
+    }
+
+    //Returns true if the player have the required resources in his strongBox or Warehouse
+    public boolean isBuyable(Verificator verificator){
+        for (PhysicalResource res : price){
+            if(!verificator.verifyResources(res))
+                return false;
+        }
+        return true;
+    }
 }
