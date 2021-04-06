744d94f1be6f2a775ba266cf6f8597ab1ce81308
FedericoCaspani <federico1.caspani@mail.polimi.it>
2021-04-06 10:40:36 +0200

    Resource implementation

--- src/main/java/it/polimi/ingsw/model/essentials/Resource.java
+++ src/main/java/it/polimi/ingsw/model/essentials/Resource.java
@@ -1,4 +1,11 @@
 package it.polimi.ingsw.model.essentials;
 
+import it.polimi.ingsw.model.exceptions.InvalidAddFaithException;
+import it.polimi.ingsw.model.match.player.Adder;
+
 public interface Resource {
+
+    //This metod add the resource to the StrongBox or increse the faitMarker
+
+    boolean add(Adder adder) throws InvalidAddFaithException;
 }
