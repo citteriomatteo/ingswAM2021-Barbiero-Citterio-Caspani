b1652d414e53bdc376765087c19da9cb60b67c29
FedericoCaspani <federico1.caspani@mail.polimi.it>
2021-04-06 11:03:53 +0200

    Verificator interface

--- src/main/java/it/polimi/ingsw/model/exceptions/InvalidAddFaithException.java
+++ src/main/java/it/polimi/ingsw/model/exceptions/InvalidAddFaithException.java
@@ -1,4 +1,9 @@
 package it.polimi.ingsw.model.exceptions;
 
 public class InvalidAddFaithException extends Exception {
+    private String error;
+
+    public InvalidAddFaithException() {
+        this.error = "";
+    }
 }
--- src/main/java/it/polimi/ingsw/model/match/player/Verificator.java
+++ src/main/java/it/polimi/ingsw/model/match/player/Verificator.java
@@ -1,4 +1,17 @@
 package it.polimi.ingsw.model.match.player;
 
+import it.polimi.ingsw.model.essentials.CardColor;
+import it.polimi.ingsw.model.essentials.CardType;
+import it.polimi.ingsw.model.essentials.PhysicalResource;
+
 public interface Verificator {
+
+    //this method verify the presence of the PhisicalResources in the wherhouse or in the StrongBox
+    boolean verifyResources(PhysicalResource physicalResource);
+
+    //this method verify the presence of a card with the indicated color
+    boolean verifyColor(CardColor color);
+
+    ////this method verify the presence of a card with the indicated color and level
+    boolean verifyCard(CardType card);
 }
