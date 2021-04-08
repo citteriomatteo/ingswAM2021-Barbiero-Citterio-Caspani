b44c920d5fd0121c6ff10c6c55ecccf107f110f8
FedericoCaspani <federico1.caspani@mail.polimi.it>
2021-04-08 10:05:34 +0200

    New Exception

---
+++ src/main/java/it/polimi/ingsw/model/exceptions/InvalidQuantityException.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.model.exceptions;
+
+public class InvalidQuantityException extends Exception {
+    private String error;
+
+    public InvalidQuantityException(String error) {
+        this.error = error;
+    }
+}
