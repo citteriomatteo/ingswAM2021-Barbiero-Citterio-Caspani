65dd89860aef5b9e432f1d3aec25a9eea5c84bcf
FedericoCaspani <federico1.caspani@mail.polimi.it>
2021-04-13 19:11:52 +0200

    CardComparator

---
+++ src/main/java/it/polimi/ingsw/model/essentials/leader/CardComparator.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.model.essentials.leader;
+
+import it.polimi.ingsw.model.essentials.CardColor;
+import it.polimi.ingsw.model.essentials.DevelopmentCard;
+
+import java.util.Comparator;
+
+public class CardComparator implements Comparator<DevelopmentCard> {
+
+    @Override
+    public int compare(DevelopmentCard devCard1, DevelopmentCard devCard2) {
+        if(devCard1.getType().getLevel() < devCard2.getType().getLevel() ||
+                (devCard1.getType().getLevel() == devCard2.getType().getLevel() && devCard1.getType().getColor().getVal() < devCard2.getType().getColor().getVal()))
+            return -1;
+        else
+            return 1;
+    }
+}
