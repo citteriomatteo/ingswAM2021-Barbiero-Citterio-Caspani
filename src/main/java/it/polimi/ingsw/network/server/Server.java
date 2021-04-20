f5c59c7bae15d9d94a534f48899520156d7302e1
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2021-04-20 15:45:54 +0200

    - Server Client first step

---
+++ src/main/java/it/polimi/ingsw/network/server/Server.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw.network.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void startServer() {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        ServerSocket serverSocket;
+        try {
+            serverSocket = new ServerSocket(port);
+        }
+        catch (IOException e) {
+            System.err.println(e.getMessage()); // porta non disponibile
+            return;
+        }
+        System.out.println("Server ready");
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                System.out.println("new client accepted");
+                executor.submit(new ServerClientHandler(socket));
+            }
+            catch(IOException e) {
+                 break;  //entrerei qui se serverSocket venisse chiuso
+            }
+         }
+             executor.shutdown();
+    }
+    public static void main(String[] args) {
+        Server echoServer = new Server(1337);
+        echoServer.startServer();
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/network/server/ServerClientHandler.java
@@ -0,0 +1,36 @@
+package it.polimi.ingsw.network.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerClientHandler implements Runnable {
+    private Socket socket;
+
+    public ServerClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream()); // leggo e scrivo nella connessione finche' non ricevo "quit"
+            while (true) {
+                String line = in.nextLine();
+                if (line.equals("quit")) {
+                    break;
+                } else {
+                    out.println("Received: " + line);
+                    out.flush();
+                }
+            } // chiudo gli stream e il socket
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
+
