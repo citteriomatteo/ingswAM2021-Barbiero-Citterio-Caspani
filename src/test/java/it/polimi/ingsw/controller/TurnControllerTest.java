eb5eaf7b868b9b22d40e945c3735038e82f42828
FedericoCaspani <federico1.caspani@mail.polimi.it>
2021-04-28 15:01:50 +0200

    fix TurnControllerTest

--- src/test/java/it/polimi/ingsw/controller/TurnControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/TurnControllerTest.java
@@ -6,14 +6,18 @@ import it.polimi.ingsw.model.essentials.ResType;
 import it.polimi.ingsw.model.essentials.leader.LeaderCard;
 import it.polimi.ingsw.model.essentials.leader.SlotEffect;
 import it.polimi.ingsw.model.essentials.leader.WhiteMarbleEffect;
+import it.polimi.ingsw.model.exceptions.InvalidOperationException;
 import it.polimi.ingsw.model.exceptions.NegativeQuantityException;
 import it.polimi.ingsw.model.exceptions.SingleMatchException;
 import it.polimi.ingsw.model.exceptions.WrongSettingException;
 import it.polimi.ingsw.model.match.Match;
 import it.polimi.ingsw.model.match.MultiMatch;
 import it.polimi.ingsw.model.match.player.Player;
+import it.polimi.ingsw.model.match.player.personalBoard.warehouse.Warehouse;
+import it.polimi.ingsw.model.match.player.personalBoard.warehouse.WarehouseDecorator;
 import it.polimi.ingsw.network.message.ctosmessage.LeaderActivationMessage;
 import it.polimi.ingsw.network.message.ctosmessage.LeaderDiscardingMessage;
+import it.polimi.ingsw.network.message.ctosmessage.SwitchShelfMessage;
 import it.polimi.ingsw.network.message.ctosmessage.WhiteMarbleConversionMessage;
 import org.junit.jupiter.api.Test;
 
@@ -88,61 +92,60 @@ public class TurnControllerTest {
 
         Player p = match.getCurrentPlayer();
         p.setHandLeaders(new LinkedList<>(Arrays.asList(
-                new LeaderCard(Arrays.asList(new PhysicalResource(ResType.STONE,0)),5,new WhiteMarbleEffect(new PhysicalResource(ResType.STONE,1))),
-                new LeaderCard(Arrays.asList(new PhysicalResource(ResType.STONE,0)),5,new WhiteMarbleEffect(new PhysicalResource(ResType.COIN,1)))
-                )));
+                new LeaderCard(Arrays.asList(new PhysicalResource(ResType.STONE, 0)), 5, new WhiteMarbleEffect(new PhysicalResource(ResType.STONE, 1))),
+                new LeaderCard(Arrays.asList(new PhysicalResource(ResType.STONE, 0)), 5, new WhiteMarbleEffect(new PhysicalResource(ResType.COIN, 1)))
+        )));
 
-        p.activateLeader(p.getHandLeaders().get(0)); p.activateLeader(p.getHandLeaders().get(0));
+        p.activateLeader(p.getHandLeaders().get(0));
+        p.activateLeader(p.getHandLeaders().get(0));
 
         controller.setCurrentState(StateName.MARKET_ACTION);
         controller.setWhiteMarbleDrawn(3);
 
-        List<PhysicalResource> whiteResources = new LinkedList<>(Arrays.asList(new PhysicalResource(ResType.STONE,1), new PhysicalResource(ResType.SHIELD,3), new PhysicalResource(ResType.COIN,1)));
+        List<PhysicalResource> whiteResources = new LinkedList<>(Arrays.asList(new PhysicalResource(ResType.STONE, 1), new PhysicalResource(ResType.SHIELD, 3), new PhysicalResource(ResType.COIN, 1)));
 
         controller.newOperation(new WhiteMarbleConversionMessage(p.getNickname(), whiteResources));
         assertEquals(1, p.getPersonalBoard().getWarehouse().getBuffer().size());
 
-        whiteResources.remove(1); whiteResources.remove(1);
+        whiteResources.remove(1);
+        whiteResources.remove(1);
         controller.newOperation(new WhiteMarbleConversionMessage(p.getNickname(), whiteResources));
         assertEquals(1, p.getPersonalBoard().getWarehouse().getBuffer().size());
 
-        whiteResources.add(new PhysicalResource(ResType.STONE,1));
+        whiteResources.add(new PhysicalResource(ResType.STONE, 1));
         controller.newOperation(new WhiteMarbleConversionMessage(p.getNickname(), whiteResources));
         assertEquals(3, p.getPersonalBoard().getWarehouse().getBuffer().size());
 
-
-    @Test
-    public void testSwitchShelf() throws NegativeQuantityException, InvalidOperationException, WrongSettingException, SingleMatchException {
-        initialization();
-        PhysicalResource resource = new PhysicalResource(ResType.SERVANT,2);
-        PhysicalResource resource1 = new PhysicalResource(ResType.SHIELD,2);
-        PhysicalResource resource2 = new PhysicalResource(ResType.COIN,1);
-        match.getCurrentPlayer().addToWarehouse(resource);
-        match.getCurrentPlayer().addToWarehouse(resource1);
-        match.getCurrentPlayer().moveIntoWarehouse(new PhysicalResource(ResType.SERVANT,2),2);
-        match.getCurrentPlayer().moveIntoWarehouse(new PhysicalResource(ResType.SHIELD,2),3);
-
-        controller.newOperation(new SwitchShelfMessage("player1",2,3));
-        assertTrue(match.getCurrentPlayer().getPersonalBoard().getWarehouse().getWarehouseDisposition().get(1).equals(resource1));
-        assertTrue(match.getCurrentPlayer().getPersonalBoard().getWarehouse().getWarehouseDisposition().get(2).equals(resource));
-
-        match.getCurrentPlayer().addToWarehouse(resource2);
-        match.getCurrentPlayer().moveIntoWarehouse(resource2,1);
-
-        Warehouse oldWh = WarehouseDecorator.clone(match.getCurrentPlayer().getPersonalBoard().getWarehouse());
-
-        controller.newOperation(new SwitchShelfMessage("player1",8,1));
-        assertEquals(oldWh.getWarehouseDisposition(),match.getCurrentPlayer().getPersonalBoard().getWarehouse().getWarehouseDisposition());
-
-        controller.newOperation(new SwitchShelfMessage("player1",2,1));
-        assertEquals(oldWh.getWarehouseDisposition(),match.getCurrentPlayer().getPersonalBoard().getWarehouse().getWarehouseDisposition());
-
-
     }
+        @Test
+        public void testSwitchShelf () throws
+        NegativeQuantityException, InvalidOperationException, WrongSettingException, SingleMatchException {
+            initialization();
+            PhysicalResource resource = new PhysicalResource(ResType.SERVANT, 2);
+            PhysicalResource resource1 = new PhysicalResource(ResType.SHIELD, 2);
+            PhysicalResource resource2 = new PhysicalResource(ResType.COIN, 1);
+            match.getCurrentPlayer().addToWarehouse(resource);
+            match.getCurrentPlayer().addToWarehouse(resource1);
+            match.getCurrentPlayer().moveIntoWarehouse(new PhysicalResource(ResType.SERVANT, 2), 2);
+            match.getCurrentPlayer().moveIntoWarehouse(new PhysicalResource(ResType.SHIELD, 2), 3);
+
+            controller.newOperation(new SwitchShelfMessage("player1", 2, 3));
+            assertTrue(match.getCurrentPlayer().getPersonalBoard().getWarehouse().getWarehouseDisposition().get(1).equals(resource1));
+            assertTrue(match.getCurrentPlayer().getPersonalBoard().getWarehouse().getWarehouseDisposition().get(2).equals(resource));
+
+            match.getCurrentPlayer().addToWarehouse(resource2);
+            match.getCurrentPlayer().moveIntoWarehouse(resource2, 1);
+
+            Warehouse oldWh = WarehouseDecorator.clone(match.getCurrentPlayer().getPersonalBoard().getWarehouse());
+
+            controller.newOperation(new SwitchShelfMessage("player1", 8, 1));
+            assertEquals(oldWh.getWarehouseDisposition(), match.getCurrentPlayer().getPersonalBoard().getWarehouse().getWarehouseDisposition());
+
+            controller.newOperation(new SwitchShelfMessage("player1", 2, 1));
+            assertEquals(oldWh.getWarehouseDisposition(), match.getCurrentPlayer().getPersonalBoard().getWarehouse().getWarehouseDisposition());
 
 
-
-
+        }
 
 
     public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
