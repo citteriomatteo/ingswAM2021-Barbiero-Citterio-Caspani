7801c9fe8f05b55255feda816c44316a8dd9c1bb
matteocitterio <10620055@polimi.it>
2021-05-14 13:18:17 +0200

    help map of client commands

--- src/main/java/it/polimi/ingsw/network/client/KeyboardReader.java
+++ src/main/java/it/polimi/ingsw/network/client/KeyboardReader.java
@@ -6,8 +6,10 @@ import it.polimi.ingsw.network.message.ctosmessage.LoginMessage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-
+import java.util.stream.Collectors;
 
 import static java.util.Map.entry;
 
@@ -17,7 +19,23 @@ public class KeyboardReader extends Thread{
     private static final Map<String, String> helpMap;
     static{
         helpMap = Map.ofEntries(
-                entry("login", "[Nickname]")
+                entry("login", "[Nickname]"),
+                entry("selection", "[y/n]"),
+                entry("numPlayers","[numPlayers]"),
+                entry("leadersChoice","[LeaderID]"),
+                entry("startingResource","[ResourceType,Quantity]"),
+                entry("switchShelf","[firstShelf,secondShelf]"),
+                entry("leaderActivation","[LeaderID]"),
+                entry("leaderDiscarding","[LeaderID]"),
+                entry("marketDraw","[row?,number]"),
+                entry("whiteMarblesConversion","[ResourceType,Quantity]"),
+                entry("warehouseInsertion","[SingleResourceType,Shelf]"),
+                entry("devCardDraw","[RowNumber,ColumnNumber]"),
+                entry("payments","[-warehouse Shelf,ResourceType,Quantity -strongbox ResourceType,Quantity]"),
+                entry("devCardPlacement","[Column]"),
+                entry("production","[-cardsid cardID -ucosts ResourceType,Quantity -uearnings ResourceType,Quantity]"),
+                entry("endTurn","[end_turn]"),
+                entry("rematch","[y/n]")
         );
     }
 
@@ -55,5 +73,15 @@ public class KeyboardReader extends Thread{
                 e.printStackTrace();
             }
         }
+    }
+
+
+    public void printHelpMap(Map<String, String> map){
+
+        List<String> helpKeys = new ArrayList<>(map.keySet());
+        List<String> helpValues = new ArrayList<>(map.values());
+
+        for(int i = 0; i < helpKeys.size(); i++)
+            System.out.println(i + ".  "+helpKeys.get(i) + " - " + helpValues.get(i));
     }
 }
