519f0461264d99162761aaebb26762672c0f6d5b
FedericoCaspani <federico1.caspani@mail.polimi.it>
2021-06-14 21:24:17 +0200

    javaDoc

--- src/main/java/it/polimi/ingsw/view/GUI/GoodbyeSceneController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/GoodbyeSceneController.java
@@ -4,14 +4,23 @@ import javafx.scene.control.Label;
 
 import static it.polimi.ingsw.view.GUI.SceneProxy.getSceneProxy;
 
+/**
+ * A scene controller used to used for communicating with the goodbye scene.
+ */
 public class GoodbyeSceneController implements SceneController {
-
     public Label errorMessage;
 
+    /**
+     * Creates an instance and links it to the {@link SceneProxy}.
+     */
     public GoodbyeSceneController() {
         getSceneProxy().setGoodbyePhaseSceneController(this);
     }
 
+    /**
+     * Prints the goodbye message on the scene
+     * @param msg the goodbye message.
+     */
     public void printGoodbyeMessage(String msg) {
         errorMessage.setText(msg);
     }
--- src/main/java/it/polimi/ingsw/view/GUI/SceneController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/SceneController.java
@@ -1,6 +1,4 @@
 package it.polimi.ingsw.view.GUI;
 
 public interface SceneController {
-
-
 }
